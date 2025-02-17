1b125e1c322179c2e0539fd18f041cdd5d128269
Prosto-Filya <m2112626@edu.misis.ru>
2025-02-17 14:51:04 +0300

    Добавлены комментарии над методами и классами

--- painting_cost_calculator.py
+++ painting_cost_calculator.py
@@ -1,5 +1,6 @@
 # painting_cost_calculator.py
 
+# Класс для расчета стоимости покраски автомобиля в зависимости от детали и цвета.
 class PaintingCostCalculator:
     # Базовая стоимость покраски
     BASE_COST = 12000
@@ -8,11 +9,12 @@ class PaintingCostCalculator:
     COLOR_COEFFICIENTS = { "белый": 1, "синий": 1, "желтый": 1.1, "красный": 1, "перламутровый": 1.2, "серый металлик": 1.3 }
 
     PART_COEFFICIENTS = { "капот": 1, "передняя дверь": 1.2, "задняя дверь": 1.1, "передний бампер": 1, "задний бампер": 1, "крыша": 1.1 }
-
+# Инициализация экземпляра класса.
     def __init__(self, part, color):
         self.part = part.lower()
         self.color = color.lower()
 
+# Проверяет, существуют ли введенные деталь и цвет в соответствующих словарях.
     def validate_input(self):
         if self.part not in self.PART_COEFFICIENTS:
             print("Ошибка: Деталь не найдена в списке.")
@@ -21,7 +23,7 @@ class PaintingCostCalculator:
             print("Ошибка: Цвет не найден в списке.")
             return False
         return True
-
+# Рассчитывает стоимость покраски на основе выбранной детали и цвета.
     def calculate_cost(self):
         if not self.validate_input():
             return None
